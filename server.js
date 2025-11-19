2863dbbf3f6b62af58163fc1f80ee0fc3aa8e67f
Sebas <sb43423@gmail.com>
2025-11-19 11:16:18 -0500

    eexpress

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
